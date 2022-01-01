c7fb5908fac1568774ac27e927df46f2f14be2b8
thepresidentm <miguelanva9@gmail.com>
2022-01-01 17:28:27 -0600

    variables

---
+++ index.js
@@ -0,0 +1,8 @@
+const https = require('https');
+const keys = require('./keys');
+
+// Datos
+const token = keys.token;
+const facebookURL = 'https://graph.facebook.com/v12.0/';
+const fortniteURL =  'https://fortnite-api.com/v2/shop/br?language=es-419';
+
