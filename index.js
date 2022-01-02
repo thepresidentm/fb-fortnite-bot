122ed6b4d589b70b1c70cc9e7e72528d473fe8bd
thepresidentm <miguelanva9@gmail.com>
2022-01-02 15:35:29 -0600

    execute every minute

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const fortnite = require('./fortnite');
+var cron = require('node-cron');
 
-fortnite.execute();
+cron.schedule('* * * * *', () =>  {
+    let date = new Date();
+    console.log(date.getMinutes());
+    fortnite.execute();
+});
