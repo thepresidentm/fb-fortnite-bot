a081b08c20ff08fb1577ef1d13819b5c5c345864
thepresidentm <miguelanva9@gmail.com>
2022-01-02 00:05:43 -0600

    better

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const keys = require('./keys');
 const fortnite = require('./fortnite');
-
-// Datos
-const token = keys.token;
-const facebookURL = 'https://graph.facebook.com/v12.0/';
 
 fortnite.execute();
