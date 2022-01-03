82cfdb10fd2e87320afa5898e4e90a3dbc978be2
thepresidentm <miguelanva9@gmail.com>
2022-01-02 20:12:46 -0600

    default route

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const express = require('express');
 const fortnite = require('./fortnite');
 var cron = require('node-cron');
+
+// Logic
 
 cron.schedule('* * * * *', () =>  {
     let date = new Date();
@@ -8,4 +11,15 @@ cron.schedule('* * * * *', () =>  {
 }, {
     scheduled: true,
     timezone: "America/Mexico_City"
+});
+
+// Web
+
+const app = express();
+const port = process.env.PORT || "5000";
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+    res.send("<h1>Hola Mundo</h1>");
 });
